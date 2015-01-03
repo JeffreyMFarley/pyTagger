73cdbd52fdc4215c69d7b505bdc8b99b00e5133f
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-01-03 16:09:16 -0500

    Added tags and command line argument support

--- pyTagger/mp3_snapshot.py
+++ pyTagger/mp3_snapshot.py
@@ -8,6 +8,7 @@ if sys.version < '3':
     import eyed3
     from eyed3 import main, mp3, id3, core
     import codecs
+import argparse
 import binascii
 import hashlib
 
@@ -17,38 +18,61 @@ import hashlib
 
 class Formatter():
     projectionEyed3 = {
-        'id': lambda x: Formatter.extractTaggerId(x),
-        'track': lambda x: x.tag.track_num[0], 
-        'totalTrack': lambda x: x.tag.track_num[1],
-        'title': lambda x: x.tag.title,
         'album': lambda x: x.tag.album, 
-        'artist': lambda x: x.tag.artist,
         'albumArtist': lambda x: x.tag.album_artist, 
+        'artist': lambda x: x.tag.artist,
+        'bitRate': lambda x: x.info.bit_rate[1] if x.info != None else None, 
+        'bpm': lambda x: x.tag.bpm,
+        'comments': lambda x: [{'lang':y.lang, 'text':y.text, 'description': y.description} for y in x.tag.comments],
+        'compilation': lambda x: x.tag.getTextFrame('TCMP'),
+        'composer': lambda x: x.tag.getTextFrame('TCOM'),
+        'conductor': lambda x: x.tag.getTextFrame('TPE3'),
+        'disc': lambda x: x.tag.disc_num[0],
+        'encodingDate': lambda x: Formatter.extractDate(x.tag.encoding_date),
+        'fileHash': lambda x: '',
         'genre': lambda x: x.tag.genre.name if x.tag.genre else '', 
         'group': lambda x: x.tag.getTextFrame('TIT1'),
-        'subtitle': lambda x: x.tag.getTextFrame('TIT3'),
-        'disc': lambda x: x.tag.disc_num[0],
-        'totalDisc': lambda x: x.tag.disc_num[1],
-        'publisher': lambda x: x.tag.publisher,
-        'bpm': lambda x: x.tag.bpm,
+        'id': lambda x: Formatter.extractTaggerId(x),
         'key': lambda x: x.tag.getTextFrame('TKEY'),
+        'language': lambda x: x.tag.getTextFrame('TLAN'),
+        'length' : lambda x: x.info.time_secs if x.info else '',
+        'lyrics': lambda x: [{'lang':y.lang, 'text':y.text, 'description': y.description} for y in x.tag.lyrics],
+        'media': lambda x: x.tag.getTextFrame('TMED'),
+        'originalReleaseDate': lambda x: Formatter.extractDate(x.tag.original_release_date),
         'playCount': lambda x: x.tag.play_count,
-        'ufid': lambda x: {y.owner_id: binascii.b2a_base64(y.uniq_id).strip() for y in x.tag.unique_file_ids},
-        'comments': lambda x: [{'lang':y.lang, 'text':y.text, 'description': y.description} for y in x.tag.comments],
-        'bitRate': lambda x: x.info.bit_rate[1] if x.info != None else None, 
+        'publisher': lambda x: x.tag.publisher,
+        'recordingDate': lambda x: Formatter.extractDate(x.tag.recording_date),
+        'releaseDate': lambda x: Formatter.extractDate(x.tag.release_date),
+        'remixer': lambda x: x.tag.getTextFrame('TPE4'),
+        'subtitle': lambda x: x.tag.getTextFrame('TIT3'),
+        'taggingDate': lambda x: Formatter.extractDate(x.tag.tagging_date),
+        'title': lambda x: x.tag.title,
+        'totalDisc': lambda x: x.tag.disc_num[1],
+        'totalTrack': lambda x: x.tag.track_num[1],
+        'track': lambda x: x.tag.track_num[0], 
+        'ufid': lambda x: Formatter.extractFileIds(x),
         'vbr': lambda x: x.info.bit_rate[0] if x.info != None else None,
-        'fileHash': lambda x: ''
+        'year': lambda x: Formatter.extractDate(x.tag.getBestDate()),
     }
     columns = list(projectionEyed3.keys())
+
+    basic = ['title', 'track', 'totalTrack', 'artist', 'albumArtist', 'album', 'length']
+    songwriting = ['bpm', 'composer', 'key', 'lyrics', 'language']
+    production = ['year', 'releaseDate', 'originalReleaseDate', 'recordingDate', 'conductor', 'remixer']
+    distribution = ['media', 'disc', 'totalDisc']
+    library = ['genre', 'id', 'ufid', 'compilation', 'comments', 'playCount', 'group', 'subtitle', 'taggingDate']
+    mp3Info = ['bitRate', 'vbr', 'fileHash']
 
     def __init__(self, fieldSet=columns):
         self.fieldSet = fieldSet
 
     def format(self, obj):
-         if isinstance(obj, mp3.Mp3AudioFile) and obj.tag:
-             return { k: self.projectionEyed3[k](obj) for k in self.fieldSet } 
-         # Let the base class default method raise the TypeError
-         return {}
+        if isinstance(obj, mp3.Mp3AudioFile) and obj.tag:
+            row = {}
+            for k in self.fieldSet: 
+                row[k] = self.projectionEyed3[k](obj)  # Python 2.7 Mac does not like dictionary comprehensions
+            return row;
+        return {}
 
     @classmethod
     def extractTaggerId(cls, track):
@@ -57,6 +81,17 @@ class Formatter():
             if a0.owner_id == 'DJTagger' :
                 ufid = binascii.b2a_base64(a0.uniq_id).strip()
         return ufid
+
+    @classmethod
+    def extractFileIds(cls, track):
+        ids = {}
+        for x in track.tag.unique_file_ids:
+            ids[x.owner_id] = binascii.b2a_base64(x.uniq_id).strip()  # Python 2.7 Mac does not like dictionary comprehensions
+        return ids
+
+    @classmethod
+    def extractDate(cls, date):
+        return date.year if date else ''
 
 class Mp3Snapshot:
     def __init__(self):
@@ -81,7 +116,11 @@ class Mp3Snapshot:
 
                     # Check if the file has an extension of typical music files
                     if self.currentPath[-3:].lower() in ['mp3']:
-                        print("Processing", self.currentPath)
+                        try:
+                            print("Processing", self.currentPath)
+                        except UnicodeEncodeError:
+                            print("Processing", self.currentPath.encode('ascii', errors='replace'))
+
                         if self.loadID3():
                             a = formatter.format(self.track)
                             if 'fileHash' in fieldSet:
@@ -126,23 +165,59 @@ class Mp3Snapshot:
         with codecs.open(fileName, 'r', encoding='utf-8') as f:
             return json.load(f)
 
+    def save(self, fileName, object):
+        with codecs.open(fileName, 'w', encoding='utf-8') as f:
+            return json.dump(object, f, indent=2)
 
 # debugging
-#sys.argv = [sys.argv[0], r'C:\dvp\MP3Tools\SampleData', r'..\data\snapshot.json']
+#sys.argv = [sys.argv[0], '-b', '-l', r'C:\dvp\MP3Tools\SampleData', r'C:\Users\Jeff\Documents\East Wind\snapshot.json']
+#sys.argv = [sys.argv[0], '--help']
+sys.argv = [sys.argv[0], r'J:\Jeff Music\Music\Justin Timberlake\The 20_20 Experience', r'J:\mp3s.json']
 
 #-------------------------------------------------------------------------------
 # Main
 #-------------------------------------------------------------------------------
 
+def buildArgParser():
+    p = argparse.ArgumentParser(description='Scan directories and build a snapshot of the MP3s')
+    p.add_argument('path',  nargs='?', metavar='path',
+                   default=os.getcwd(),
+                   help='the path to scan')
+    p.add_argument('outfile',  nargs='?', metavar='outfile',
+                   default='mp3s.json',
+                   help='the name of the file that will hold the results')
+    p.add_argument('-b', '--basic', action='store_true', dest='basic',
+                   help=' '.join(Formatter.basic))
+    p.add_argument('-s', '--songwriting', action='store_true', dest='songwriting',
+                   help=' '.join(Formatter.songwriting))
+    p.add_argument('-p', '--production', action='store_true', dest='production',
+                   help=' '.join(Formatter.production))
+    p.add_argument('-d', '--distribution', action='store_true', dest='distribution',
+                   help=' '.join(Formatter.distribution))
+    p.add_argument('-l', '--library', action='store_true', dest='library',
+                   help=' '.join(Formatter.library))
+    p.add_argument('-m', '--mp3Info', action='store_true', dest='mp3Info',
+                   help=' '.join(Formatter.mp3Info))
+    p.add_argument('-a', '--all', action='store_true', dest='all',
+                   help='include all supported fields')
+
+    return p
+
 if __name__ == '__main__':
-    path = os.getcwd()
-    outFileName = 'mp3s.json'
-    
-    argc = len(sys.argv)
-    if argc > 2:
-        outFileName = sys.argv[2]
-    if argc > 1:
-        path = sys.argv[1]
+    parser = buildArgParser()
+    args = parser.parse_args()
+
+    columns = []
+    if args.basic:  columns = columns + Formatter.basic
+    if args.songwriting:  columns = columns + Formatter.songwriting
+    if args.production:  columns = columns + Formatter.production
+    if args.distribution:  columns = columns + Formatter.distribution
+    if args.library:  columns = columns + Formatter.library
+    if args.mp3Info:  columns = columns + Formatter.mp3Info
+    if args.all: columns = Formatter.columns
+
+    if not columns:
+        columns = Formatter.basic
 
     pipeline = Mp3Snapshot()
-    pipeline.createFromScan(path, outFileName)
+    pipeline.createFromScan(args.path, args.outfile, list(set(columns)))
