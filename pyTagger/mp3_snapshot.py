b65b34ed3f2ad323cc94baaaa71fbc036b2b707d
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-01-15 15:28:28 -0500

    Meeting PEP8 Standards

--- pyTagger/mp3_snapshot.py
+++ pyTagger/mp3_snapshot.py
@@ -4,6 +4,9 @@ from __future__ import print_function
 import json
 import os
 import sys
+import argparse
+import binascii
+import hashlib
 if sys.version < '3':
     import eyed3
     from eyed3 import main, mp3, id3, core
@@ -13,37 +16,43 @@ if sys.version < '3':
 else:
     _input = lambda fileName: open(fileName, 'r', encoding='utf-8')
     _output = lambda fileName: open(fileName, 'w', encoding='utf-8')
-import argparse
-import binascii
-import hashlib
 
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Classes
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 class Formatter():
     projectionEyed3 = {
-        'album': lambda x: x.tag.album, 
-        'albumArtist': lambda x: x.tag.album_artist, 
+        'album': lambda x: x.tag.album,
+        'albumArtist': lambda x: x.tag.album_artist,
         'artist': lambda x: x.tag.artist,
-        'bitRate': lambda x: x.info.bit_rate[1] if x.info != None else None, 
+        'bitRate':
+        lambda x: x.info.bit_rate[1] if x.info is not None else None,
         'bpm': lambda x: x.tag.bpm,
-        'comments': lambda x: [{'lang':y.lang, 'text':y.text, 'description': y.description} for y in x.tag.comments],
+        'comments': lambda x: [{'lang': y.lang,
+                                'text': y.text,
+                                'description': y.description}
+                               for y in x.tag.comments],
         'compilation': lambda x: x.tag.getTextFrame('TCMP'),
         'composer': lambda x: x.tag.getTextFrame('TCOM'),
         'conductor': lambda x: x.tag.getTextFrame('TPE3'),
         'disc': lambda x: x.tag.disc_num[0],
         'encodingDate': lambda x: Formatter.extractDate(x.tag.encoding_date),
         'fileHash': lambda x: '',
-        'genre': lambda x: x.tag.genre.name if x.tag.genre else '', 
+        'genre': lambda x: x.tag.genre.name if x.tag.genre else '',
         'group': lambda x: x.tag.getTextFrame('TIT1'),
         'id': lambda x: Formatter.extractTaggerId(x),
         'key': lambda x: x.tag.getTextFrame('TKEY'),
         'language': lambda x: x.tag.getTextFrame('TLAN'),
-        'length' : lambda x: x.info.time_secs if x.info else '',
-        'lyrics': lambda x: [{'lang':y.lang, 'text':y.text, 'description': y.description} for y in x.tag.lyrics],
+        'length': lambda x: x.info.time_secs if x.info else '',
+        'lyrics': lambda x: [{'lang': y.lang,
+                              'text': y.text,
+                              'description': y.description}
+                             for y in x.tag.lyrics],
         'media': lambda x: x.tag.getTextFrame('TMED'),
-        'originalReleaseDate': lambda x: Formatter.extractDate(x.tag.original_release_date),
+        'originalReleaseDate':
+        lambda x: Formatter.extractDate(x.tag.original_release_date),
         'playCount': lambda x: x.tag.play_count,
         'publisher': lambda x: x.tag.publisher,
         'recordingDate': lambda x: Formatter.extractDate(x.tag.recording_date),
@@ -54,18 +63,21 @@ class Formatter():
         'title': lambda x: x.tag.title,
         'totalDisc': lambda x: x.tag.disc_num[1],
         'totalTrack': lambda x: x.tag.track_num[1],
-        'track': lambda x: x.tag.track_num[0], 
+        'track': lambda x: x.tag.track_num[0],
         'ufid': lambda x: Formatter.extractFileIds(x),
-        'vbr': lambda x: x.info.bit_rate[0] if x.info != None else None,
+        'vbr': lambda x: x.info.bit_rate[0] if x.info is not None else None,
         'year': lambda x: Formatter.extractDate(x.tag.getBestDate()),
     }
     columns = list(projectionEyed3.keys())
 
-    basic = ['title', 'track', 'totalTrack', 'artist', 'albumArtist', 'album', 'length']
+    basic = ['title', 'track', 'totalTrack', 'artist',
+             'albumArtist', 'album', 'length']
     songwriting = ['bpm', 'composer', 'key', 'lyrics', 'language']
-    production = ['year', 'releaseDate', 'originalReleaseDate', 'recordingDate', 'conductor', 'remixer', 'publisher']
+    production = ['year', 'releaseDate', 'originalReleaseDate',
+                  'recordingDate', 'conductor', 'remixer', 'publisher']
     distribution = ['media', 'disc', 'totalDisc']
-    library = ['genre', 'id', 'ufid', 'compilation', 'comments', 'playCount', 'group', 'subtitle', 'encodingDate', 'taggingDate']
+    library = ['genre', 'id', 'ufid', 'compilation', 'comments', 'playCount',
+               'group', 'subtitle', 'encodingDate', 'taggingDate']
     mp3Info = ['bitRate', 'vbr', 'fileHash']
 
     def __init__(self, fieldSet=columns):
@@ -73,67 +85,77 @@ class Formatter():
 
     def format(self, obj):
         if isinstance(obj, mp3.Mp3AudioFile) and obj.tag:
+            # Python 2.6 does not like dictionary comprehensions
             row = {}
-            for k in self.fieldSet: 
-                row[k] = self.projectionEyed3[k](obj)  # Python 2.7 Mac does not like dictionary comprehensions
-            return row;
+            for k in self.fieldSet:
+                row[k] = self.projectionEyed3[k](obj)
+            return row
         return {}
 
     @classmethod
     def extractTaggerId(cls, track):
         ufid = ''
         for a0 in track.tag.unique_file_ids:
-            if a0.owner_id == 'DJTagger' :
+            if a0.owner_id == 'DJTagger':
                 ufid = binascii.b2a_base64(a0.uniq_id).strip()
         return ufid
 
     @classmethod
     def extractFileIds(cls, track):
+        # Python 2.6 does not like dictionary comprehensions
         ids = {}
         for x in track.tag.unique_file_ids:
-            ids[x.owner_id] = binascii.b2a_base64(x.uniq_id).strip()  # Python 2.7 Mac does not like dictionary comprehensions
+            ids[x.owner_id] = binascii.b2a_base64(x.uniq_id).strip()
         return ids
 
     @classmethod
     def extractDate(cls, date):
         return date.year if date else ''
 
+
 class Mp3Snapshot:
     def __init__(self):
         self.currentPath = ''
 
-    def createFromScan(self, scanPath, outFileName, fieldSet=Formatter.columns):
+    def createFromScan(self, scanPath, outFileName,
+                       fieldSet=Formatter.columns):
         formatter = Formatter(fieldSet)
 
         try:
             fout = _output(outFileName)
             fout.writelines('{')
             sep = ''
+            osCoding = sys.getfilesystemencoding()
 
             for currentDir, subdirs, files in os.walk(scanPath):
                 # Get the absolute path of the currentDir parameter
                 currentDir = os.path.abspath(currentDir)
-     
+
                 # Traverse through all files
                 for fileName in files:
                     self.currentPath = os.path.join(currentDir, fileName)
-                    self.currentPath = self.currentPath.decode(sys.getfilesystemencoding())
+                    self.currentPath = self.currentPath.decode(osCoding)
 
                     # Check if the file has an extension of typical music files
                     if self.currentPath[-3:].lower() in ['mp3']:
                         try:
                             print("Processing", self.currentPath)
                         except UnicodeEncodeError:
-                            print("Processing", self.currentPath.encode('ascii', errors='replace'))
+                            print("Processing",
+                                  self.currentPath.encode('ascii',
+                                                          errors='replace'))
 
                         if self.loadID3():
                             a = formatter.format(self.track)
                             if 'fileHash' in fieldSet:
                                 a['fileHash'] = self.calculateHash()
-                            fout.writelines([sep,'"',self.currentPath.replace('\\', '\\\\'),'":'])
+                            fout.writelines([sep, '"',
+                                             self.currentPath.replace('\\',
+                                                                      '\\\\'),
+                                             '":'])
                             json.dump(a, fout, indent=2)
                             sep = ','
-                
+
         finally:
             fout.writelines('}')
             fout.close()
@@ -145,10 +167,12 @@ class Mp3Snapshot:
         except (IOError, ValueError):
             print('Error with ID3 Load', self.currentPath, file=sys.stderr)
             return False
-        
+
     def calculateHash(self):
         chunk_size = 1024
-        offset = self.track.tag.header.tag_size if self.track.tag and self.track.tag.header else 0
+        offset = (self.track.tag.header.tag_size
+                  if self.track.tag and self.track.tag.header
+                  else 0)
         shaAccum = hashlib.sha1()
         try:
             with open(self.currentPath, "rb") as f:
@@ -160,11 +184,11 @@ class Mp3Snapshot:
         except IOError:
             print('Cannot Hash', self.currentPath, file=sys.stderr)
             return ''
-        return binascii.b2a_base64(shaAccum.digest()).strip();
+        return binascii.b2a_base64(shaAccum.digest()).strip()
 
-    #-------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
     # I/O
-    #-------------------------------------------------------------------------------
+    # -------------------------------------------------------------------------
 
     def load(self, fileName):
         with _input(fileName) as f:
@@ -174,14 +198,19 @@ class Mp3Snapshot:
         with _output(fileName) as f:
             return json.dump(object, f, indent=2)
 
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Main
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#sys.argv = [sys.argv[0], r'C:\dvp\MP3Tools\SampleData', r'C:\Users\Jeff\Documents\East Wind\snapshot.json', '--all']
+# sys.argv = [sys.argv[0],
+#            r'C:\dvp\MP3Tools\SampleData',
+#            r'C:\Users\Jeff\Documents\East Wind\snapshot.json',
+#            '--all']
+
 
 def buildArgParser():
-    p = argparse.ArgumentParser(description='Scan directories and build a snapshot of the MP3s')
+    description = 'Scan directories and build a snapshot of the MP3s'
+    p = argparse.ArgumentParser(description=description)
     p.add_argument('path',  nargs='?', metavar='path',
                    default=os.getcwd(),
                    help='the path to scan')
@@ -190,12 +219,12 @@ def buildArgParser():
                    help='the name of the file that will hold the results')
     p.add_argument('-b', '--basic', action='store_true', dest='basic',
                    help=' '.join(Formatter.basic))
-    p.add_argument('-s', '--songwriting', action='store_true', dest='songwriting',
-                   help=' '.join(Formatter.songwriting))
-    p.add_argument('-p', '--production', action='store_true', dest='production',
-                   help=' '.join(Formatter.production))
-    p.add_argument('-d', '--distribution', action='store_true', dest='distribution',
-                   help=' '.join(Formatter.distribution))
+    p.add_argument('-s', '--songwriting', action='store_true',
+                   dest='songwriting', help=' '.join(Formatter.songwriting))
+    p.add_argument('-p', '--production', action='store_true',
+                   dest='production', help=' '.join(Formatter.production))
+    p.add_argument('-d', '--distribution', action='store_true',
+                   dest='distribution', help=' '.join(Formatter.distribution))
     p.add_argument('-l', '--library', action='store_true', dest='library',
                    help=' '.join(Formatter.library))
     p.add_argument('-m', '--mp3Info', action='store_true', dest='mp3Info',
@@ -210,13 +239,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     columns = []
-    if args.basic:  columns = columns + Formatter.basic
-    if args.songwriting:  columns = columns + Formatter.songwriting
-    if args.production:  columns = columns + Formatter.production
-    if args.distribution:  columns = columns + Formatter.distribution
-    if args.library:  columns = columns + Formatter.library
-    if args.mp3Info:  columns = columns + Formatter.mp3Info
-    if args.all: columns = Formatter.columns
+    if args.basic:
+        columns = columns + Formatter.basic
+    if args.songwriting:
+        columns = columns + Formatter.songwriting
+    if args.production:
+        columns = columns + Formatter.production
+    if args.distribution:
+        columns = columns + Formatter.distribution
+    if args.library:
+        columns = columns + Formatter.library
+    if args.mp3Info:
+        columns = columns + Formatter.mp3Info
+    if args.all:
+        columns = Formatter.columns
 
     if not columns:
         columns = Formatter.basic
--- pyTagger/snapshot_converter.py
+++ pyTagger/snapshot_converter.py
@@ -13,15 +13,12 @@ if sys.version < '3':
 else:
     _input = lambda fileName: open(fileName, 'r', encoding='utf-8')
     _output = lambda fileName: open(fileName, 'w', encoding='utf-8')
+from pyTagger.mp3_snapshot import Formatter
 
-if __package__ is None:
-    from mp3_snapshot import Formatter
-else:
-    from .mp3_snapshot import Formatter
-
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Classes
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+
 
 class SnapshotConverter:
     def __init__(self):
@@ -31,18 +28,20 @@ class SnapshotConverter:
         with _input(inFileName) as f:
             snapshot = json.load(f)
 
-        # build the columns if they are not supplied 
+        # build the columns if they are not supplied
         if not fieldSet:
-            fieldSet = self.extractColumns(snapshot)
+            fieldSet = self._extractColumns(snapshot)
         fieldSet.append('fullPath')
-        
+
         with _output(outFileName) as f:
-            writer = csv.DictWriter(f, fieldSet, dialect=csv.excel if useCsv else csv.excel_tab, extrasaction='ignore')
+            dialect = csv.excel if useCsv else csv.excel_tab
+            writer = csv.DictWriter(f, fieldSet, dialect=dialect,
+                                    extrasaction='ignore')
             writer.writeheader()
-            for k,v in snapshot.items():
+            for k, v in snapshot.items():
                 row = v
                 row['fullPath'] = k
-                try:        
+                try:
                     writer.writerow(row)
                 except UnicodeError:
                     for field in fieldSet:
@@ -50,7 +49,7 @@ class SnapshotConverter:
                             row[field] = row[field].encode('ascii', 'replace')
                     writer.writerow(row)
 
-    def extractColumns(self, data):
+    def _extractColumns(self, data):
         header = set()
 
         for k, v in data.items():
@@ -59,7 +58,7 @@ class SnapshotConverter:
                     header.add(j)
 
         # Build the ordered set with the extra columns at the end
-        known = orderedAllColumns()
+        known = self._orderedAllColumns()
         unknown = header - set(known)
 
         columns = [c for c in known if c in header]
@@ -68,45 +67,44 @@ class SnapshotConverter:
 
         return columns
 
-#-------------------------------------------------------------------------------
-# Helper function
-#-------------------------------------------------------------------------------
+    @staticmethod
+    def _orderedAllColumns():
+        # preserve order
+        columns = (Formatter.basic +
+                   Formatter.songwriting +
+                   Formatter.production +
+                   Formatter.distribution +
+                   Formatter.library +
+                   Formatter.mp3Info)
 
-def orderedAllColumns():
-    # preserve order
-    columns = Formatter.basic + \
-              Formatter.songwriting + \
-              Formatter.production + \
-              Formatter.distribution + \
-              Formatter.library + \
-              Formatter.mp3Info
-        
-    # for testing that all fields are grouped
-    missing = set(Formatter.columns) - set(columns)
-    assert(not missing)
-    return columns
+        # for testing that all fields are grouped
+        missing = set(Formatter.columns) - set(columns)
+        assert(not missing)
+        return columns
 
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 # Main
-#-------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
-#sys.argv = [sys.argv[0], r'C:\dvp\Mp3Reduce\data\mp3s_enh.json', 
+# sys.argv = [sys.argv[0], r'C:\dvp\Mp3Reduce\data\mp3s_enh.json',
 #                         r'C:\Users\Jeff\Documents\East Wind\snapshot.txt']
 
+
 def buildArgParser():
-    p = argparse.ArgumentParser(description='Convert MP3 snapshot to a row and column format')
+    description = 'Convert MP3 snapshot to a row and column format'
+    p = argparse.ArgumentParser(description=description)
     p.add_argument('infile', metavar='infile', help='the snapshot to process')
     p.add_argument('outfile',  nargs='?', metavar='outfile',
                    default='mp3s.txt',
                    help='the name of the file that will hold the results')
     p.add_argument('-b', '--basic', action='store_true', dest='basic',
                    help=' '.join(Formatter.basic))
-    p.add_argument('-s', '--songwriting', action='store_true', dest='songwriting',
-                   help=' '.join(Formatter.songwriting))
-    p.add_argument('-p', '--production', action='store_true', dest='production',
-                   help=' '.join(Formatter.production))
-    p.add_argument('-d', '--distribution', action='store_true', dest='distribution',
-                   help=' '.join(Formatter.distribution))
+    p.add_argument('-s', '--songwriting', action='store_true',
+                   dest='songwriting', help=' '.join(Formatter.songwriting))
+    p.add_argument('-p', '--production', action='store_true',
+                   dest='production', help=' '.join(Formatter.production))
+    p.add_argument('-d', '--distribution', action='store_true',
+                   dest='distribution', help=' '.join(Formatter.distribution))
     p.add_argument('-l', '--library', action='store_true', dest='library',
                    help=' '.join(Formatter.library))
     p.add_argument('-m', '--mp3Info', action='store_true', dest='mp3Info',
@@ -114,7 +112,7 @@ def buildArgParser():
     p.add_argument('-a', '--all', action='store_true', dest='all',
                    help='include all supported fields')
     p.add_argument('--csv', action='store_true', dest='csv',
-                   help='use CSV as the output format (tab-delimited is the default)')
+                   help='CSV as the output format (default = tab-delimited)')
 
     return p
 
@@ -123,13 +121,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     columns = []
-    if args.basic:  columns = columns + Formatter.basic
-    if args.songwriting:  columns = columns + Formatter.songwriting
-    if args.production:  columns = columns + Formatter.production
-    if args.distribution:  columns = columns + Formatter.distribution
-    if args.library:  columns = columns + Formatter.library
-    if args.mp3Info:  columns = columns + Formatter.mp3Info
-    if args.all:    columns = orderedAllColumns() 
-    
+    if args.basic:
+        columns = columns + Formatter.basic
+    if args.songwriting:
+        columns = columns + Formatter.songwriting
+    if args.production:
+        columns = columns + Formatter.production
+    if args.distribution:
+        columns = columns + Formatter.distribution
+    if args.library:
+        columns = columns + Formatter.library
+    if args.mp3Info:
+        columns = columns + Formatter.mp3Info
+    if args.all:
+        columns = SnapshotConverter._orderedAllColumns()
+
     pipeline = SnapshotConverter()
     pipeline.convert(args.infile, args.outfile, columns, args.csv)
