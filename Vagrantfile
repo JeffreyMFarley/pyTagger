ba85909aad2c1ce4adb77f44774aa157a1ed32fe
JeffreyMFarley <JeffreyMFarley@users.noreply.github.com>
2016-09-20 13:05:52 -0400

    Initial Elasticsearch configuration

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.box_check_update = false
 
-  #config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 9200, host: 9200
 
   config.vm.synced_folder ".", "/home/vagrant"
 
---
+++ elasticsearch/bot.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Check that networking is up.
+[ "$NETWORKING" = "no" ] && exit 0
+
+# -----------------------------------------------------------------------------
+
+image_name=es
+docker_tag="elasticsearch"
+
+# -----------------------------------------------------------------------------
+
+run() {
+    docker run -d -p "9200:9200" \
+    --name "$image_name" "$docker_tag" && \
+    sleep 10 && \
+    curl -XPUT 'localhost:9200/library' -d @/home/vagrant/elasticsearch/mapping.json
+    return $?
+}
+
+stop() {
+    docker stop "$image_name" && docker rm "$image_name"
+}
+
+# -----------------------------------------------------------------------------
+
+case $1 in
+    run)
+        run
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+        echo "Usage: $0 {run | test}"
+        exit 2
+        ;;
+esac
