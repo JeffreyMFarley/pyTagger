2d130b0d25e487adefb6a78b5570416b77f16a36
JeffreyMFarley <JeffreyMFarley@users.noreply.github.com>
2016-10-03 15:11:56 -0400

    Find similar names

--- pyTagger/operations/find_duplicates.py
+++ pyTagger/operations/find_duplicates.py
@@ -1,26 +1,32 @@
+from __future__ import print_function
+from pyTagger.io import loadJson, toAbsolute
 from pyTagger.proxies.es import Client
-from pyTagger.mp3_snapshot import Formatter
+
+# -----------------------------------------------------------------------------
+# Clones = Exact Duplicates
 
 
-def _rowsFromBuckets(buckets):
+def _flattenAggregation(response):
+    buckets = response[u"aggregations"][u"primary"][u"buckets"]
+
     for bucket in buckets:
         key = bucket[u"key"]
-        for dups in bucket[u"files"][u"buckets"]:
-            yield (key, dups[u"key"])
+        for secondary in bucket[u"secondary"][u"buckets"]:
+            yield (key, secondary[u"key"])
 
 
-def findDuplicates(client):
+def findClones(client):
     r = client.search({
         'from': 0, 'size': 0,
         'aggs': {
-            'duplicates': {
+            'primary': {
                 'terms': {
                     'field': 'fileHash',
                     'min_doc_count': 2,
                     'size': 100
                 },
                 'aggs': {
-                    'files': {
+                    'secondary': {
                         'terms': {
                             'field': 'path'
                         }
@@ -30,14 +36,90 @@ def findDuplicates(client):
         }
     })
 
-    buckets = r[u"aggregations"][u"duplicates"][u"buckets"]
+    return _flattenAggregation(r)
 
-    return _rowsFromBuckets(buckets)
+# -----------------------------------------------------------------------------
+# Isonom = Name based matches
+
+
+def _isonomQuery(track):
+        # "_source": [
+        #     "title", "track", "artist", "albumArtist", "album",
+        #     "length", "subtitle", "genre", "path", "disc", "totalDisc"
+        # ],
+
+    terms = []
+
+    if 'track' in track and track['track']:
+        terms.append({"term": {"track": track['track']}})
+
+    for k in ['album', 'artist', 'title']:
+        if k in track and track[k]:
+            terms.append({
+                "match": {k: {"query": track[k], "operator": "and"}}
+            })
+
+    if len(terms) == 0:
+        raise ValueError("track does not contain any searchable fields")
+
+    query = {
+        "from": 0, "size": 10,
+        "sort": [
+            "_score",
+            "album.raw"
+        ],
+        "query": {
+            "bool": {
+                "should": terms
+            }
+        },
+        "min_score": 4
+    }
+
+    return query
+
+
+def _projectIsonomResults(response):
+    hits = response[u"hits"][u"hits"]
+    for hit in hits:
+        yield (hit[u"_score"], hit[u"_source"])
+
+
+def findIsonomTracks(client, track):
+    query = _isonomQuery(track)
+    response = client.search(query)
+    for score, isonom in _projectIsonomResults(response):
+        yield (track, score, isonom)
+
+# -----------------------------------------------------------------------------
+
 
 if __name__ == "__main__":
-    cli = Client()
+    from hew import Normalizer
+    import sys
+    import json
 
-    fmt = Formatter()
-    print("hash,file")
-    for hashValue, fileName in findDuplicates(cli):
-        print("{0},{1}".format(hashValue, fmt.normalizeToAscii(fileName)))
+    snapshot = loadJson(toAbsolute('../mp3s-cfpb.json'))
+
+    cli = Client()
+    nml = Normalizer()
+
+    print("file,score,isonom")
+    for k, v in snapshot.items():
+        matches, near = 0, 0
+        try:
+            for _, score, isonom in findIsonomTracks(cli, v):
+                if score > 10:
+                    print("{0},{1},{2}".format(
+                        nml.to_ascii(k), score, nml.to_ascii(isonom['path']))
+                    )
+                    matches += 1
+                else:
+                    near += 1
+        except Exception as e:
+            print("Error on", nml.to_ascii(k), e,
+                  json.dumps(v, indent=2, sort_keys=True),
+                  file=sys.stderr, sep='\n', end='\n\n')
+        if matches != 1:
+            print("Match Error", nml.to_ascii(k), matches, near,
+                  file=sys.stderr)
--- tests/operations/find_duplicates_tests.py
+++ tests/operations/find_duplicates_tests.py
@@ -1,23 +1,24 @@
 import unittest
-from pyTagger.operations.find_duplicates import findDuplicates
+import json
+import pyTagger.operations.find_duplicates as sut
 try:
     from unittest.mock import patch, Mock
 except ImportError:
     from mock import patch, Mock
 
 
-class TestFindDuplicates(unittest.TestCase):
+class TestFindClones(unittest.TestCase):
     @patch('pyTagger.operations.find_duplicates.Client', spec=True)
-    def test_findDuplicates(self, client):
+    def test_findClones(self, client):
         client.search.return_value = {
-            u"aggregations": {
-                u"duplicates": {
-                    u"buckets": [{
-                        u"key": u"foo",
-                        u"files": {
-                            u"buckets": [
-                                {u"key": u"bar"},
-                                {u"key": u"baz"},
+            u'aggregations': {
+                u'primary': {
+                    u'buckets': [{
+                        u'key': u'foo',
+                        u'secondary': {
+                            u'buckets': [
+                                {u'key': u'bar'},
+                                {u'key': u'baz'},
                             ]
                         }
                     }]
@@ -25,12 +26,101 @@ class TestFindDuplicates(unittest.TestCase):
             }
         }
 
-        actual = list(findDuplicates(client))
+        actual = list(sut.findClones(client))
         self.assertEqual(client.search.call_count, 1)
         self.assertEqual(actual, [
             (u'foo', u'bar'),
             (u'foo', u'baz')
         ])
+
+
+class TestFindIsonoms(unittest.TestCase):
+    def setUp(self):
+        self.fixture = {
+            'album': u'Bar',
+            'artist': u'Foo (with Qaz)',
+            'track': 5,
+            'totalTrack': 14,
+            'compilation': None,
+            'title': u'Baz',
+            'albumArtist': u'Foo',
+            'totalDisc': 1,
+            'disc': 1
+        }
+
+    def test_isonomQuery_empty(self):
+        with self.assertRaises(ValueError):
+            sut._isonomQuery({})
+
+    def test_isonomQuery_missingTrackNumber(self):
+        del self.fixture['track']
+        actual = sut._isonomQuery(self.fixture)
+        terms = json.dumps(actual['query']['bool']['should'])
+        self.assertIn('artist', terms)
+        self.assertNotIn('track', terms)
+
+    def test_isonomQuery_nullTrackNumber(self):
+        self.fixture['track'] = None
+        actual = sut._isonomQuery(self.fixture)
+        terms = json.dumps(actual['query']['bool']['should'])
+        self.assertIn('artist', terms)
+        self.assertNotIn('track', terms)
+
+    def test_isonomQuery_missingTitle(self):
+        del self.fixture['title']
+        actual = sut._isonomQuery(self.fixture)
+        terms = json.dumps(actual['query']['bool']['should'])
+        self.assertIn('track', terms)
+        self.assertNotIn('title', terms)
+
+    def test_isonomQuery_nullTitle(self):
+        self.fixture['title'] = None
+        actual = sut._isonomQuery(self.fixture)
+        terms = json.dumps(actual['query']['bool']['should'])
+        self.assertIn('track', terms)
+        self.assertNotIn('title', terms)
+
+    def test_isonomQuery_missingAlbum(self):
+        del self.fixture['album']
+        actual = sut._isonomQuery(self.fixture)
+        terms = json.dumps(actual['query']['bool']['should'])
+        self.assertIn('title', terms)
+        self.assertNotIn('album', terms)
+
+    def test_isonomQuery_emptyAlbum(self):
+        self.fixture['album'] = ''
+        actual = sut._isonomQuery(self.fixture)
+        terms = json.dumps(actual['query']['bool']['should'])
+        self.assertIn('title', terms)
+        self.assertNotIn('album', terms)
+
+    def test_isonomQuery_missingArtist(self):
+        del self.fixture['artist']
+        actual = sut._isonomQuery(self.fixture)
+        terms = json.dumps(actual['query']['bool']['should'])
+        self.assertIn('album', terms)
+        self.assertNotIn('artist', terms)
+
+    @patch('pyTagger.operations.find_duplicates.Client', spec=True)
+    def test_findIsonomTracks(self, client):
+        client.search.return_value = {
+            "hits": {
+                "hits": [{
+                    "_score": 10.1,
+                    "_source": {"foo": "bar"}
+                }],
+                "max_score": 10.1,
+                "total": 9999
+            }
+        }
+
+        gen = sut.findIsonomTracks(client, self.fixture)
+        actual = list(gen)
+        self.assertEqual(client.search.call_count, 1)
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(actual[0][0], self.fixture)
+        self.assertEqual(actual[0][1], 10.1)
+        self.assertEqual(actual[0][2], {"foo": "bar"})
 
 if __name__ == '__main__':
     unittest.main()
