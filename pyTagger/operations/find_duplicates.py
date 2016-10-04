8fc86f536f94f1d760f5b36e243487207c4897ec
JeffreyMFarley <JeffreyMFarley@users.noreply.github.com>
2016-10-04 11:26:08 -0400

    Find similar names in snapshot

--- pyTagger/operations/find_duplicates.py
+++ pyTagger/operations/find_duplicates.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+from __future__ import unicode_literals
+from collections import namedtuple
 from pyTagger.io import loadJson, toAbsolute
 from pyTagger.proxies.es import Client
 
@@ -7,12 +9,12 @@ from pyTagger.proxies.es import Client
 
 
 def _flattenAggregation(response):
-    buckets = response[u"aggregations"][u"primary"][u"buckets"]
+    buckets = response['aggregations']['primary']['buckets']
 
     for bucket in buckets:
-        key = bucket[u"key"]
-        for secondary in bucket[u"secondary"][u"buckets"]:
-            yield (key, secondary[u"key"])
+        key = bucket['key']
+        for secondary in bucket['secondary']['buckets']:
+            yield (key, secondary['key'])
 
 
 def findClones(client):
@@ -41,85 +43,95 @@ def findClones(client):
 # -----------------------------------------------------------------------------
 # Isonom = Name based matches
 
+Isonom = namedtuple('Isonom', [
+    'status', 'oldPath', 'newPath', 'score', 'oldTags', 'newTags'
+])
+
 
 def _isonomQuery(track):
-        # "_source": [
-        #     "title", "track", "artist", "albumArtist", "album",
-        #     "length", "subtitle", "genre", "path", "disc", "totalDisc"
-        # ],
+    query = {
+        'from': 0, 'size': 10,
+        'sort': '_score',
+        'query': {'bool': {}},
+        'min_score': 4
+    }
+
+    if 'id' in track and track['id']:
+        query['query']['bool']['must_not'] = {'term': {'id': track['id']}}
 
     terms = []
 
     if 'track' in track and track['track']:
-        terms.append({"term": {"track": track['track']}})
+        terms.append({'term': {'track': track['track']}})
 
     for k in ['album', 'artist', 'title']:
         if k in track and track[k]:
             terms.append({
-                "match": {k: {"query": track[k], "operator": "and"}}
+                'match': {k: {'query': track[k], 'operator': 'and'}}
+            })
+            terms.append({
+                'match': {k: {'query': track[k]}}
             })
 
     if len(terms) == 0:
-        raise ValueError("track does not contain any searchable fields")
+        raise ValueError('track does not contain any searchable fields')
 
-    query = {
-        "from": 0, "size": 10,
-        "sort": [
-            "_score",
-            "album.raw"
-        ],
-        "query": {
-            "bool": {
-                "should": terms
-            }
-        },
-        "min_score": 4
-    }
-
+    query['query']['bool']['should'] = terms
     return query
 
 
 def _projectIsonomResults(response):
-    hits = response[u"hits"][u"hits"]
+    hits = response['hits']['hits']
     for hit in hits:
-        yield (hit[u"_score"], hit[u"_source"])
+        yield (hit['_score'], hit['_source'])
 
 
 def findIsonomTracks(client, track):
     query = _isonomQuery(track)
     response = client.search(query)
     for score, isonom in _projectIsonomResults(response):
-        yield (track, score, isonom)
+        path = isonom['path']
+        del isonom['path']
+        yield (path, score, isonom)
+
+
+def findIsonoms(client, snapshot):
+    for k in sorted(snapshot):
+        v = snapshot[k]
+
+        try:
+            matches = list(findIsonomTracks(client, v))
+            quality = [x for x in matches if x[1] >= 12]
+
+            if len(quality) == 1:
+                path, score, tags = quality[0]
+                yield Isonom('single', k, path, score, v, tags)
+            elif len(matches) == 1:
+                path, score, tags = matches[0]
+                yield Isonom('single', k, path, score, v, tags)
+            elif len(matches) > 1:
+                for path, score, tags in matches:
+                    yield Isonom('multiple', k, path, score, v, tags)
+            else:
+                yield Isonom('nothing', k, None, 0.0, v, None)
+        except ValueError:
+            yield Isonom('insufficient', k, None, 0.0, v, None)
 
 # -----------------------------------------------------------------------------
 
 
-if __name__ == "__main__":
-    from hew import Normalizer
-    import sys
+if __name__ == '__main__':
+    import io
     import json
 
-    snapshot = loadJson(toAbsolute('../mp3s-cfpb.json'))
-
+    snapshot = loadJson(toAbsolute('../mp3s-add-to-library.json'))
     cli = Client()
-    nml = Normalizer()
+    sep = '\n'
 
-    print("file,score,isonom")
-    for k, v in snapshot.items():
-        matches, near = 0, 0
-        try:
-            for _, score, isonom in findIsonomTracks(cli, v):
-                if score > 10:
-                    print("{0},{1},{2}".format(
-                        nml.to_ascii(k), score, nml.to_ascii(isonom['path']))
-                    )
-                    matches += 1
-                else:
-                    near += 1
-        except Exception as e:
-            print("Error on", nml.to_ascii(k), e,
-                  json.dumps(v, indent=2, sort_keys=True),
-                  file=sys.stderr, sep='\n', end='\n\n')
-        if matches != 1:
-            print("Match Error", nml.to_ascii(k), matches, near,
-                  file=sys.stderr)
+    with io.open('isonoms.json', 'w', encoding='utf-8') as f:
+        f.write('[')
+        for row in findIsonoms(cli, snapshot):
+            f.write(sep)
+            f.write(unicode(json.dumps(row.__dict__, ensure_ascii=False)))
+            sep = ',\n'
+        f.write('\n]')
--- tests/operations/find_duplicates_tests.py
+++ tests/operations/find_duplicates_tests.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import unittest
 import json
 import pyTagger.operations.find_duplicates as sut
@@ -11,15 +12,15 @@ class TestFindClones(unittest.TestCase):
     @patch('pyTagger.operations.find_duplicates.Client', spec=True)
     def test_findClones(self, client):
         client.search.return_value = {
-            u'aggregations': {
-                u'primary': {
-                    u'buckets': [{
-                        u'key': u'foo',
-                        u'secondary': {
-                            u'buckets': [
-                                {u'key': u'bar'},
-                                {u'key': u'baz'},
-                            ]
+            'aggregations': {
+                'primary': {
+                    'buckets': [{
+                        'key': 'foo',
+                        'secondary': {
+                            'buckets': [
+                                {'key': 'bar'},
+                                {'key': 'baz'}
+                                ]
                         }
                     }]
                 }
@@ -29,23 +30,34 @@ class TestFindClones(unittest.TestCase):
         actual = list(sut.findClones(client))
         self.assertEqual(client.search.call_count, 1)
         self.assertEqual(actual, [
-            (u'foo', u'bar'),
-            (u'foo', u'baz')
+            ('foo', 'bar'),
+            ('foo', 'baz')
         ])
 
 
 class TestFindIsonoms(unittest.TestCase):
     def setUp(self):
         self.fixture = {
-            'album': u'Bar',
-            'artist': u'Foo (with Qaz)',
+            'album': 'Bar',
+            'artist': 'Foo (with Qaz)',
             'track': 5,
             'totalTrack': 14,
             'compilation': None,
-            'title': u'Baz',
-            'albumArtist': u'Foo',
+            'title': 'Baz',
+            'albumArtist': 'Foo',
             'totalDisc': 1,
-            'disc': 1
+            'disc': 1,
+            'id': 'barfoo'
+        }
+        self.qualityMatch = {
+            'hits': {
+                'hits': [{
+                    '_score': 12.1,
+                    '_source': {'foo': 'bar', 'path': '/path/to/foo'}
+                }],
+                'max_score': 12.1,
+                'total': 9999
+            }
         }
 
     def test_isonomQuery_empty(self):
@@ -55,72 +67,167 @@ class TestFindIsonoms(unittest.TestCase):
     def test_isonomQuery_missingTrackNumber(self):
         del self.fixture['track']
         actual = sut._isonomQuery(self.fixture)
-        terms = json.dumps(actual['query']['bool']['should'])
+        compound = actual['query']['bool']
+        terms = json.dumps(compound['should'])
+        self.assertIn('must_not', compound)
         self.assertIn('artist', terms)
         self.assertNotIn('track', terms)
 
     def test_isonomQuery_nullTrackNumber(self):
         self.fixture['track'] = None
         actual = sut._isonomQuery(self.fixture)
-        terms = json.dumps(actual['query']['bool']['should'])
+        compound = actual['query']['bool']
+        terms = json.dumps(compound['should'])
+        self.assertIn('must_not', compound)
         self.assertIn('artist', terms)
         self.assertNotIn('track', terms)
 
     def test_isonomQuery_missingTitle(self):
         del self.fixture['title']
         actual = sut._isonomQuery(self.fixture)
-        terms = json.dumps(actual['query']['bool']['should'])
+        compound = actual['query']['bool']
+        terms = json.dumps(compound['should'])
+        self.assertIn('must_not', compound)
         self.assertIn('track', terms)
         self.assertNotIn('title', terms)
 
     def test_isonomQuery_nullTitle(self):
         self.fixture['title'] = None
         actual = sut._isonomQuery(self.fixture)
-        terms = json.dumps(actual['query']['bool']['should'])
+        compound = actual['query']['bool']
+        terms = json.dumps(compound['should'])
+        self.assertIn('must_not', compound)
         self.assertIn('track', terms)
         self.assertNotIn('title', terms)
 
     def test_isonomQuery_missingAlbum(self):
         del self.fixture['album']
         actual = sut._isonomQuery(self.fixture)
-        terms = json.dumps(actual['query']['bool']['should'])
+        compound = actual['query']['bool']
+        terms = json.dumps(compound['should'])
+        self.assertIn('must_not', compound)
         self.assertIn('title', terms)
         self.assertNotIn('album', terms)
 
     def test_isonomQuery_emptyAlbum(self):
         self.fixture['album'] = ''
         actual = sut._isonomQuery(self.fixture)
-        terms = json.dumps(actual['query']['bool']['should'])
+        compound = actual['query']['bool']
+        terms = json.dumps(compound['should'])
+        self.assertIn('must_not', compound)
         self.assertIn('title', terms)
         self.assertNotIn('album', terms)
 
     def test_isonomQuery_missingArtist(self):
         del self.fixture['artist']
         actual = sut._isonomQuery(self.fixture)
-        terms = json.dumps(actual['query']['bool']['should'])
+        compound = actual['query']['bool']
+        terms = json.dumps(compound['should'])
+        self.assertIn('must_not', compound)
         self.assertIn('album', terms)
         self.assertNotIn('artist', terms)
 
+    def test_isonomQuery_missingId(self):
+        del self.fixture['id']
+        actual = sut._isonomQuery(self.fixture)
+        compound = actual['query']['bool']
+        self.assertNotIn('must_not', compound)
+
+    def test_isonomQuery_blankId(self):
+        self.fixture['id'] = ''
+        actual = sut._isonomQuery(self.fixture)
+        compound = actual['query']['bool']
+        self.assertNotIn('must_not', compound)
+
     @patch('pyTagger.operations.find_duplicates.Client', spec=True)
     def test_findIsonomTracks(self, client):
-        client.search.return_value = {
-            "hits": {
-                "hits": [{
-                    "_score": 10.1,
-                    "_source": {"foo": "bar"}
-                }],
-                "max_score": 10.1,
-                "total": 9999
-            }
-        }
-
+        client.search.return_value = self.qualityMatch
         gen = sut.findIsonomTracks(client, self.fixture)
         actual = list(gen)
         self.assertEqual(client.search.call_count, 1)
         self.assertEqual(len(actual), 1)
-        self.assertEqual(actual[0][0], self.fixture)
-        self.assertEqual(actual[0][1], 10.1)
+        self.assertEqual(actual[0][0], "/path/to/foo")
+        self.assertEqual(actual[0][1], 12.1)
         self.assertEqual(actual[0][2], {"foo": "bar"})
+
+    @patch('pyTagger.operations.find_duplicates.Client', spec=True)
+    def test_findIsonomsInsufficient(self, client):
+        client.search.side_effect = ValueError
+        gen = sut.findIsonoms(client, {'foo': self.fixture})
+        actual = list(gen)
+        self.assertEqual(client.search.call_count, 1)
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(actual[0].status, 'insufficient')
+
+    @patch('pyTagger.operations.find_duplicates.Client', spec=True)
+    def test_findIsonomsQualitySingle(self, client):
+        client.search.return_value = self.qualityMatch
+        gen = sut.findIsonoms(client, {'foo': self.fixture})
+        actual = list(gen)
+        self.assertEqual(client.search.call_count, 1)
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(actual[0].status, 'single')
+
+    @patch('pyTagger.operations.find_duplicates.Client', spec=True)
+    def test_findIsonomsSufficientSingle(self, client):
+        client.search.return_value = {
+            'hits': {
+                'hits': [{
+                    '_score': 8.1,
+                    '_source': {'foo': 'bar', 'path': '/path/to/foo'}
+                }],
+                'max_score': 8.1,
+                'total': 9999
+            }
+        }
+
+        gen = sut.findIsonoms(client, {'foo': self.fixture})
+        actual = list(gen)
+        self.assertEqual(client.search.call_count, 1)
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(actual[0].status, 'single')
+
+    @patch('pyTagger.operations.find_duplicates.Client', spec=True)
+    def test_findIsonomsMultiple(self, client):
+        client.search.return_value = {
+            'hits': {
+                'hits': [
+                    {
+                        '_score': 8.1,
+                        '_source': {'foo': 'bar', 'path': '/path/to/foo'}
+                    },
+                    {
+                        '_score': 8.1,
+                        '_source': {'foo': 'bar', 'path': '/path/to/foo'}
+                    },
+                ],
+                'max_score': 8.1,
+                'total': 9999
+            }
+        }
+
+        gen = sut.findIsonoms(client, {'foo': self.fixture})
+        actual = list(gen)
+        self.assertEqual(client.search.call_count, 1)
+        self.assertEqual(len(actual), 2)
+        self.assertEqual(actual[0].status, 'multiple')
+        self.assertEqual(actual[1].status, 'multiple')
+
+    @patch('pyTagger.operations.find_duplicates.Client', spec=True)
+    def test_findIsonomsNone(self, client):
+        client.search.return_value = {
+            'hits': {
+                'hits': [],
+                'max_score': 0.0,
+                'total': 9999
+            }
+        }
+
+        gen = sut.findIsonoms(client, {'foo': self.fixture})
+        actual = list(gen)
+        self.assertEqual(client.search.call_count, 1)
+        self.assertEqual(len(actual), 1)
+        self.assertEqual(actual[0].status, 'nothing')
 
 if __name__ == '__main__':
     unittest.main()
